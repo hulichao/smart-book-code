17337b0c2157aa0b60bb5e039582c395aa61d045
hulichao <jsu_hlc@163.com>
2018-05-27 16:46:31 +0800

    wen应用功能

--- chapter2/src/main/java/org/smart4j/chapter2/controller/CustomerDeleteServlet.java
+++ chapter2/src/main/java/org/smart4j/chapter2/controller/CustomerDeleteServlet.java
@@ -19,5 +19,6 @@ public class CustomerDeleteServlet extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // TODO
+        // FIX ME
     }
 }
